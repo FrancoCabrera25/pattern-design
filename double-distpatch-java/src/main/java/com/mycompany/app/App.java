948d804303142a153e251cd820275c165e40a136
Franco Cabrera <cabrera.franco@outlook.com>
2023-03-23 13:32:27 +0000

    refactor

--- double-distpatch-java/src/main/java/com/mycompany/app/App.java
+++ double-distpatch-java/src/main/java/com/mycompany/app/App.java
@@ -8,18 +8,24 @@ package com.mycompany.app;
 import com.mycompany.app.game.enums.GameResultEnum;
 import com.mycompany.app.game.gameClass.Paper;
 import com.mycompany.app.game.gameClass.Rock;
-import com.mycompany.app.game.gameClass.Scissor;
+// import com.mycompany.app.game.gameClass.Scissor;
 import com.mycompany.app.game.gameInterface.Hand;
 
 public class App {
     public static void main(String[] args) {
-        Hand player = new Paper();
-        Hand machine = new Rock();
+        Hand playerOne = new Paper();
+        Hand playerTwo = new Rock();
 
-        GameResultEnum ganaMachine = player.play(machine);
-        GameResultEnum ganaPlayer = machine.play(player);
-        System.out.println("Hello Remote World! " + ganaMachine.toString()) ;
+        GameResultEnum resultGame = playerOne.play(playerTwo);
 
-        System.out.println("Hello Remote World! " + ganaPlayer.toString()) ;
+        if (resultGame == GameResultEnum.WIN) {
+            System.out.println("gano Machine " + resultGame.toString());
+        } else if (resultGame == GameResultEnum.LOSE) {
+
+            System.out.println("Gano player ");
+        }else {
+            System.out.println("Empate ");
+        }
+
     }
 }
